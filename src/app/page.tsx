14cc0466d433cf238edfb1878954b139753364e0
Kleb-0 <shadowhilles@gmail.com>
2025-07-15 16:12:27 -0300

    feat: add Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
 export default function Home() {
-  return
+  return (
+    <div className="flex flex-col min-h-screen justify-center items-center">
+      <h1 className="text-3xl font-semibold">
+        Welcome to <span className="font-bold">Kleb.letter</span>
+      </h1>
+      <code className="text-muted-foreground mt-2">Calebe's personal tech blog</code>
+    </div>
+  );
 }
